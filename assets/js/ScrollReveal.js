290eb4acc3787d6b36405d344caa737e17f07af0
SingNow <117522853+singnow-17@users.noreply.github.com>
2022-12-12 17:12:14 -0300

    Update Archive
    Atualização dos novos arquivos.

---
+++ assets/js/ScrollReveal.js
@@ -0,0 +1,96 @@
+const ScrollAnimation = ScrollReveal ({
+    origin: 'right',
+    distance: '500px',
+    duration: 1000,
+    delay: 300,
+})
+
+ScrollAnimation.reveal('.card.shop');
+
+
+const ScrollAnimationTwo = ScrollReveal ({
+    origin: 'left',
+    distance: '500px',
+    duration: 1000,
+    delay: 300,
+}) 
+
+ScrollAnimationTwo.reveal('.card.contact');
+
+
+const ScrollAnimationThree = ScrollReveal ({
+    origin: 'top',
+    distance: '50px',
+    duration: 1000,
+    delay: 200,
+})
+
+ScrollAnimationThree.reveal('.nav-bar');
+
+const ScrollAnimationFor = ScrollReveal ({
+    origin: 'left',
+    distance: '250px',
+    duration: 2000,
+    delay: 400,
+})
+
+ScrollAnimationFor.reveal('.fa.fa-facebook, .fa.fa-twitter');
+
+const ScrollAnimationFive = ScrollReveal ({
+    origin: 'right',
+    distance: '250px',
+    duration: 2000,
+    delay: 400,
+})
+
+ScrollAnimationFive.reveal('.fa.fa-instagram, .fa.fa-linkedin');
+
+const ScrollAnimationSix = ScrollReveal ({
+    origin: 'top',
+    distance: '250px',
+    duration: 1000,
+    delay: 300
+})
+
+ScrollAnimationSix.reveal('.footer-content');
+
+const ScrollAnimationSeven = ScrollReveal ({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    delay: 100,
+})
+
+ScrollAnimationSeven.reveal('.footer-menu');
+
+const ScrollAnimationEigth = ScrollReveal ({
+    origin: 'top',
+    distance: '250px',
+    duration: 2000,
+    delay: 200,
+})
+
+ScrollAnimationEigth.reveal('.img-three');
+
+const ScrollAnimationNine = ScrollReveal ({
+    origin: 'top',
+    distance: '250px',
+    duration: 2000,
+    delay: 200,
+})
+
+ScrollAnimationNine.reveal('.confirm');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
